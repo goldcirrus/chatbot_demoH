e1919e233e9de946fe5072ce2104e8a60b8ac2b0
kevin zhong <53105370+goldcirrus@users.noreply.github.com>
2022-11-19 19:25:14 -0800

    message
    description

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# OpenAI API Quickstart - Python example app
+# OpenAI API - Python example app
 
-This is an example pet name generator app used in the OpenAI API [quickstart tutorial](https://beta.openai.com/docs/quickstart). It uses the [Flask](https://flask.palletsprojects.com/en/2.0.x/) web framework. Check out the tutorial or follow the instructions below to get set up.
+This is an example generator app used in the OpenAI API. It uses the [Flask](https://flask.palletsprojects.com/en/2.0.x/) web framework. Check out the tutorial or follow the instructions below to get set up.
 
 ## Setup
 
@@ -41,4 +41,4 @@ This is an example pet name generator app used in the OpenAI API [quickstart tut
    $ flask run
    ```
 
-You should now be able to access the app at [http://localhost:5000](http://localhost:5000)! For the full context behind this example app, check out the [tutorial](https://beta.openai.com/docs/quickstart).
+You should now be able to access the app at [http://localhost:5000](http://localhost:5000)! For the full context behind this example app.
--- app.py
+++ app.py
@@ -10,26 +10,47 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        userQuestion = request.form["userQ"]
         response = openai.Completion.create(
             model="text-davinci-002",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(userQuestion),
             temperature=0.6,
+            max_tokens=60,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
         )
+        
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def generate_prompt(userQuestion):
+    return """Suggest time spot and phone number for an appointment.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+schedule: morning
+available time: 9AM on September 3 and what is your call back phone number?
+
+schedule: afternoon
+available time: 1pm on September 3 and what is your call back phone number?
+
+
+as soon as possible: call back
+what is your phone number?
+
+appointment: I want to book an appointment
+The nurse can call you back at 1 pm on September 3, Monday.
+
+appointment: can I book an appointment?
+what is your call back phone number and your name?
+
+ok:that works for me
+thank you
+
+
+Phone:{}
+""".format(
+        userQuestion.capitalize()
     )
